7cccc8855f1ea2d135a8002a9f7fe3f564edd091
tatiane gardin <contatotatianegardin@hotmail.com>
2022-05-07 17:42:00 -0300

    testando alterações implementadas com base no que foi aprendido na aula

--- bytebank/src/bytebank/TestaGerente.java
+++ bytebank/src/bytebank/TestaGerente.java
@@ -14,5 +14,7 @@ public class TestaGerente {
 		boolean podeAcessar = tatianeGerente.autentica(1234);
 		System.out.println("Acesso permitido? " + podeAcessar);
 		
+		System.err.println(tatianeGerente.getBonificacao());
+		
 	}
 }
