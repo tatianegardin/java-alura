d25f72cf57c19ba82b827134f2fabe6558514a64
Tatiane Gardin <88948039+tatianegardin@users.noreply.github.com>
2022-05-09 14:22:10 -0300

    Aplicando polimorfismo (#3)
    Adicionando uma nova funcionalidade no meu gerenciamento de funcionários, com objetivo de calcular toda bonificação, com o uso de apenas um método aplicando o polimorfismo, para assim acessar os métodos de acordo com cada funcionário.
    Cada tipo de cargo possuí um calculo diferente na bonificação, e com a ajuda do polimorfismo posso acessar independe do tipo que é o objeto, levando em consideração a hierarquia.
    Usando um tipo genérico na hora de receber o parâmetro do meu método.

---
+++ bytebank/src/bytebank/ControleBonificacao.java
@@ -0,0 +1,13 @@
+package bytebank;
+
+public class ControleBonificacao {
+	private int totalBonificacaoPaga ;
+	
+	public int getTotalBonificacaoPaga() {
+		return totalBonificacaoPaga;
+	}
+	
+	public void somaBonificacao(Funcionario funcionario) {
+		this.totalBonificacaoPaga += funcionario.getBonificacao();
+	}
+}
