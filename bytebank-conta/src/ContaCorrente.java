69c2334532957a6eed8a4970b7b61bba2a0fba67
tatianegardin <contatotatianegardin@hotmail.com>
2022-05-09 23:01:10 -0300

    aplicando herança e polimorfismo

--- bytebank-conta/src/ContaCorrente.java
+++ bytebank-conta/src/ContaCorrente.java
@@ -3,4 +3,10 @@ public class ContaCorrente extends Conta {
 	public ContaCorrente(int agencia, int numero) {
 		super(agencia, numero);
 	}
+	
+	@Override
+	public boolean saca(double valor) {
+		valor += 0.2;
+		return super.saca(valor);
+	}
 }
--- bytebank-conta/src/TesteConta.java
+++ bytebank-conta/src/TesteConta.java
@@ -1,8 +1,21 @@
 
 public class TesteConta {
 	public static void main(String[] args) {
-		Conta novaConta = new Conta(0101, 145) ;
+		ContaCorrente cc = new ContaCorrente(111, 111);
+		cc.deposita(200);
 		
-		System.out.println(novaConta.getNumero());
+		ContaPoupanca cp = new ContaPoupanca(111, 222);
+		cp.deposita(200);
+		
+		//conta corrente desconta 0,20 toda vez que saca/tranfere
+		cc.transfere(50, cp);
+		
+		//conta poupança não desconta nenhum valor ao sacar/transferir
+		cp.transfere(10, cc);
+		
+		System.out.println("Saldo cc: " + cc.getSaldo());
+		System.out.println("Saldo cp: " + cp.getSaldo());
+		
+		
 	}
 }
