56513b21fd6072dc862f258ae6ce1749eaa961a7
tatianegardin <contatotatianegardin@hotmail.com>
2022-05-19 19:15:47 -0300

    criando minha primeira excessao

--- Excessoes/src/Fluxo.java
+++ Excessoes/src/Fluxo.java
@@ -1,28 +1,23 @@
-public class Main {
+public class Fluxo {
     public static void main(String[] args) {
         System.out.println("Start - main");
         try{
             metodo1();
-        } catch (ArithmeticException | NullPointerException ex){
+        } catch (ArithmeticException | NullPointerException | MinhaExcessao ex){
             System.out.println(ex.getMessage());
         }
         System.out.println("End - main");
     }
 
-    public static void metodo1(){
+    public static void metodo1  () {
         System.out.println("Start - Método 1");
         metodo2();
         System.out.println("End - Método 1");
     }
-    public static void metodo2(){
+    public static void metodo2() {
         System.out.println("Start - Método 2");
-        for (int i = 0; i <=5 ; i++){
-            System.out.println(i);
-            //int teste = 10/0;
-            Conta c= null;
-            c.deposita();
-        }
-        System.out.println("End - Método 2");
+        throw  new MinhaExcessao("Deu bem ruim");
+
     }
 
 }
---
+++ Excessoes/src/MinhaExcessao.java
@@ -0,0 +1,5 @@
+public class MinhaExcessao extends RuntimeException{
+    public MinhaExcessao(String msg){
+        super(msg);
+    }
+}
